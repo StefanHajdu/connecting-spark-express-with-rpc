7ecf46258092e525b0b8d7dea2bfc0c4d8319043
StefanHajdu <stevo.hajdu006@gmail.com>
2025-02-14 21:52:49 +0100

    multi levels of input session sharing 0000 -> 0001 -> 0002, unsuccessful test

--- curl_test/session_input_3_levels.sh
+++ curl_test/session_input_3_levels.sh
@@ -1,7 +1,4 @@
-# session 0000 is filtered for all domains that:
-#   - where tld: .com OR .org
-#   - where registrar: GoDaddy.com, LLC, NameCheap, Inc., unknown
-
+# 0000
 
 curl -X POST http://localhost:4444/createSession \
     -H 'Content-Type: application/json' \
@@ -20,68 +17,85 @@ curl -X POST http://localhost:4444/sql \
     -d '{"id": "0000", "parametrized_query": "select * from {df} where tld = '\''com'\'' OR tld = '\''org'\''", "query_name": "filter", "params_json": ["df"]}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "2.1 summarize session 0000 for tld == .com OR .org"
 curl -X POST http://localhost:4444/summarize \
     -H 'Content-Type: application/json' \
     -d '{"id": "0000"}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "3. filter session 0000 for registrar == GoDaddy.com, LLC, NameCheap, Inc., unknown"
-curl -X POST http://localhost:4444/sql \
-    -H 'Content-Type: application/json' \
-    -d '{"id": "0000", "parametrized_query": "select * from {df} where registrar = '\''GoDaddy.com, LLC'\'' OR registrar = '\''NameCheap, Inc.'\'' OR registrar = '\''unknown'\''", "query_name": "filter", "params_json": ["df"]}' \
-    -w '\nTotal: %{time_total}s\n\n'
+# 0001
 
-echo "3.1 summarize session 0000 for registrar == GoDaddy.com, LLC, NameCheap, Inc., unknown"
-curl -X POST http://localhost:4444/summarize \
-    -H 'Content-Type: application/json' \
-    -d '{"id": "0000"}' \
-    -w '\nTotal: %{time_total}s\n\n'
-
-echo "4. create session 0001"
+echo "3. create session 0001"
 curl -X POST http://localhost:4444/createSession \
     -H 'Content-Type: application/json' \
     -d '{"id": "0001"}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "5. load from session 0000"
+echo "4. load from session 0000"
 curl -X POST http://localhost:4444/loadFromSession \
     -H 'Content-Type: application/json' \
     -d '{"id": "0001", "input_id": "0000"}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "5.1 summarize load from session 0000 | SHOULD EQUAL TO 3.1"
-curl -X POST http://localhost:4444/summarize \
-    -H 'Content-Type: application/json' \
-    -d '{"id": "0001"}' \
-    -w '\nTotal: %{time_total}s\n\n'
-
-echo "6. filter session 0001 for: registrar == NameCheap, Inc."
+echo "5. filter session 0001 for: registrar == NameCheap, Inc."
 curl -X POST http://localhost:4444/sql \
     -H 'Content-Type: application/json' \
-    -d '{"id": "0001", "parametrized_query": "select * from {df} where registrar = '\''NameCheap, Inc.'\''", "query_name": "filter", "params_json": ["df"]}' \
+    -d '{"id": "0001", "parametrized_query": "select * from {df} where registrar = '\''GoDaddy.com, LLC'\'' OR registrar = '\''NameCheap, Inc.'\'' OR registrar = '\''unknown'\''", "query_name": "filter", "params_json": ["df"]}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "6.1 summarize filter session 0001 for: registrar == NameCheap, Inc. | tld == .com OR .org SHOULD BE ALSO APPLIED"
 curl -X POST http://localhost:4444/summarize \
     -H 'Content-Type: application/json' \
     -d '{"id": "0001"}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "7. filter ilter session 0000 for tld == .org"
+# 0002
+
+echo "5. create session 0002"
+curl -X POST http://localhost:4444/createSession \
+    -H 'Content-Type: application/json' \
+    -d '{"id": "0002"}' \
+    -w '\nTotal: %{time_total}s\n\n'
+
+echo "6. load from session 0001"
+curl -X POST http://localhost:4444/loadFromSession \
+    -H 'Content-Type: application/json' \
+    -d '{"id": "0002", "input_id": "0001"}' \
+    -w '\nTotal: %{time_total}s\n\n'
+
+echo "7. filter session 0002 for: registrar == NameCheap, Inc."
+curl -X POST http://localhost:4444/sql \
+    -H 'Content-Type: application/json' \
+    -d '{"id": "0002", "parametrized_query": "select * from {df} where registrar = '\''NameCheap, Inc.'\''", "query_name": "filter", "params_json": ["df"]}' \
+    -w '\nTotal: %{time_total}s\n\n'
+
+curl -X POST http://localhost:4444/summarize \
+    -H 'Content-Type: application/json' \
+    -d '{"id": "0002"}' \
+    -w '\nTotal: %{time_total}s\n\n'
+
+
+# filter 0000
+
+echo "8. filter ilter session 0000 for tld == .org"
 curl -X POST http://localhost:4444/sql \
     -H 'Content-Type: application/json' \
     -d '{"id": "0000", "parametrized_query": "select * from {df} where tld = '\''org'\''", "query_name": "filter", "params_json": ["df"]}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "7.1 filter session 0000 for tld == .org | SHOULD BE LESS THAN 3.1"
+echo "0000"
 curl -X POST http://localhost:4444/summarize \
     -H 'Content-Type: application/json' \
     -d '{"id": "0000"}' \
     -w '\nTotal: %{time_total}s\n\n'
 
-echo "8. summarize filter session 0001 for registrar == NameCheap, Inc. AND tld == org | SHOULD BE LESS THAN 7.1"
+echo "0001"
 curl -X POST http://localhost:4444/summarize \
     -H 'Content-Type: application/json' \
     -d '{"id": "0001"}' \
+    -w '\nTotal: %{time_total}s\n\n'
+
+
+echo "0002"
+curl -X POST http://localhost:4444/summarize \
+    -H 'Content-Type: application/json' \
+    -d '{"id": "0002"}' \
     -w '\nTotal: %{time_total}s\n\n'
