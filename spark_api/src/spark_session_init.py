eb6176866630e64288ce2ee85c8e2b9912af63ef
StefanHajdu <stevo.hajdu006@gmail.com>
2025-03-15 01:14:53 +0100

    applicationId

--- spark_api/src/spark_session_init.py
+++ spark_api/src/spark_session_init.py
@@ -5,5 +5,22 @@ spark = (
     SparkSession.builder.appName("SparkSession")
     .master("local[*]")
     .config("spark.driver.memory", "30720m")
+    .config("spark.scheduler.mode", "FAIR")
     .getOrCreate()
 )
+spark_app_id = spark._sc.applicationId
+
+backslash_char = "\\"
+print(rf"""
+---------------------------    
+                      _    
+                     | |   
+ ___ _ __   __ _ _ __| | __
+/ __| '_ \ / _` | '__| |/ /
+\__ \ |_) | (_| | |  |   < 
+|___/ .__/ \__,_|_|  |_|\_{backslash_char}
+    | |                    
+    |_|""")
+
+print(f"\nid: {spark_app_id}")
+print("---------------------------\n")
