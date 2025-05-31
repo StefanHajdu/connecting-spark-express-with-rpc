12be8ba73257d34e0b231cd0f94d0588de7abbd2
StefanHajdu <stevo.hajdu006@gmail.com>
2025-05-31 23:52:57 +0200

    wrap preview footer to div

--- client_app/src/components/PreviewFooter.svelte
+++ client_app/src/components/PreviewFooter.svelte
@@ -24,32 +24,34 @@ export async function forwardPreview(analysiId: string, nodeId: string): Promise
 }
 </script>
 
-<footer class="sticky bottom-0 p-1/2 flex items-center justify-between bg-gray-300">
-  <p>{title}</p>
-  <Button
-    on:click={() => {
-      dataframeTableHidden = false;
-    }}>
-    <AngleUpOutline />
-  </Button>
-  <Drawer
-    placement="bottom"
-    width="w-full"
-    transitionType="fly"
-    transitionParams={transitionParamsBottom}
-    activateClickOutside={activateClickOutside}
-    backdrop={backdrop}
-    bind:hidden={dataframeTableHidden}
-    id="sidebar8"
-    class="outline-1 outline-black">
-    <div class="mb-2 flex h-6 items-center">
-      <p>Preview</p>
-      <CloseButton
-        on:click={() => {
-          dataframeTableHidden = true;
-        }}
-        class="dark:text-white" />
-    </div>
-    <DataFrameTable bind:this={dataframeTableComponent} />
-  </Drawer>
-</footer>
+<div class="sticky bottom-0">
+  <footer class="sticky bottom-0 p-1/2 flex items-center justify-between bg-gray-300">
+    <p>{title}</p>
+    <Button
+      on:click={() => {
+        dataframeTableHidden = false;
+      }}>
+      <AngleUpOutline />
+    </Button>
+    <Drawer
+      placement="bottom"
+      width="w-full"
+      transitionType="fly"
+      transitionParams={transitionParamsBottom}
+      activateClickOutside={activateClickOutside}
+      backdrop={backdrop}
+      bind:hidden={dataframeTableHidden}
+      id="sidebar8"
+      class="outline-1 outline-black">
+      <div class="mb-2 flex h-6 items-center">
+        <p>Preview</p>
+        <CloseButton
+          on:click={() => {
+            dataframeTableHidden = true;
+          }}
+          class="dark:text-white" />
+      </div>
+      <DataFrameTable bind:this={dataframeTableComponent} />
+    </Drawer>
+  </footer>
+</div>
