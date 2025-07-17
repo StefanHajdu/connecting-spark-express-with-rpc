94b17eee4e8e2269daa271213c08ace3d50ea667
kripso <17431307+kripso@users.noreply.github.com>
2025-07-17 21:45:47 +0200

    updates ty pylance ignores

--- client_app/.svelte-kit/ambient.d.ts
+++ client_app/.svelte-kit/ambient.d.ts
@@ -100,7 +100,6 @@ declare module '$env/static/private' {
 	export const HOMEBREW_CELLAR: string;
 	export const VSCODE_GIT_ASKPASS_NODE: string;
 	export const GIT_ASKPASS: string;
-	export const GIT_PAGER: string;
 	export const npm_node_execpath: string;
 	export const npm_config_prefix: string;
 	export const COLORTERM: string;
@@ -210,7 +209,6 @@ declare module '$env/dynamic/private' {
 		HOMEBREW_CELLAR: string;
 		VSCODE_GIT_ASKPASS_NODE: string;
 		GIT_ASKPASS: string;
-		GIT_PAGER: string;
 		npm_node_execpath: string;
 		npm_config_prefix: string;
 		COLORTERM: string;
